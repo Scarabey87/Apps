311e0e2974bf4b9594350afbf6e0c866b4ae66b1
jbw87 <caravagio2323@gmail.com>
2023-10-19 15:03:27 +0300

    dd

---
+++ EM/index.js
@@ -0,0 +1,19 @@
+// const dotenv = require('dotenv')
+// dotenv.config()
+
+// console.log(process.env.PORT)
+// console.log(process.env.NODE_ENV)
+
+// console.log(Math.random(5,50))
+// console.log(process.argv)
+
+// const siteURL = 'http://127.0.0.1:5500/'
+// const url = new URL(siteURL)
+//
+// console.log(url)
+
+const chalk = require('chalk')
+
+
+const text = require('./data')
+console.log(chalk.blue(text))
